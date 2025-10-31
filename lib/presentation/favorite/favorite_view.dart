b039655e4ee6def16eb66546fc63c30c852bae32
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-10-31 20:50:29 -0300

    feat: added expanded photo, delete and delete all features

--- lib/presentation/favorite/favorite_view.dart
+++ lib/presentation/favorite/favorite_view.dart
@@ -1,42 +1,72 @@
 import 'package:coffee_app_vgv/presentation/favorite/export_favorite.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:share_plus/share_plus.dart';
 
-class FavoriteView extends StatelessWidget {
+import 'widgets/image_visualization.dart';
+
+class FavoriteView extends StatefulWidget {
   const FavoriteView({super.key});
 
   @override
+  State<FavoriteView> createState() => _FavoriteViewState();
+}
+
+class _FavoriteViewState extends State<FavoriteView> {
+  FavoriteCubit get favoriteCubit => context.read<FavoriteCubit>();
+
+  @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(title: const Text('Favorite View')),
-    body: BlocConsumer<FavoriteCubit, FavoriteState>(
-      listener: (context, state) {
-        // TODO: implement listener
-      },
+    appBar: AppBar(
+      actionsPadding: EdgeInsets.only(right: 8.0),
+      title: const Text('Favorite View'),
+      actions: [
+        GestureDetector(
+          onTap: () => context.read<FavoriteCubit>().clearFavorites(),
+          child: const Icon(Icons.delete_forever),
+        ),
+      ],
+    ),
+    body: BlocBuilder<FavoriteCubit, FavoriteState>(
       builder: (context, state) {
         return switch (state) {
           InitialFavoriteState() => const Center(
             child: Text('You have no favorites yet.'),
           ),
-          LoadFavoriteState() => ListView.builder(
+          LoadFavoriteState() => GridView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ),
             itemCount: state.favorites.length,
             itemBuilder: (context, index) {
               final coffee = state.favorites[index];
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                
-                //TODO: Add dismissible to remove favorite https://docs.flutter.dev/cookbook/gestures/dismissible
-                child: Card(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Text('Coffee: ${index + 1} '),
-                      SizedBox(
-                        height: 100,
-                        width: 100,
-                        child: Image.network(coffee.file),
+              return Ink.image(
+                fit: BoxFit.cover,
+                image: AssetImage(coffee.localPath!),
+                child: InkWell(
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => BlocProvider.value(
+                        value: favoriteCubit,
+                        child: ImageVisualization(localPath: coffee.localPath!),
                       ),
-                    ],
+                    ),
                   ),
+                  onLongPress: () async {
+                    final params = ShareParams(
+                      text: 'Have you tried this one?',
+                      files: [XFile(coffee.localPath!)],
+                    );
+
+                    final result = await SharePlus.instance.share(params);
+
+                    if (result.status == ShareResultStatus.dismissed) {
+                      print('Did you not like the pictures?');
+                    }
+                  },
                 ),
               );
             },
