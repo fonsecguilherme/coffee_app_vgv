54d8b764bbecc4ea66dd2f73774bf0791510c1e4
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-10-31 20:52:07 -0300

    refactor: implemented use of local storage

--- lib/presentation/favorite/cubit/favorite_cubit.dart
+++ lib/presentation/favorite/cubit/favorite_cubit.dart
@@ -1,18 +1,24 @@
+import 'package:coffee_app_vgv/data/datasource/local_coffee_datasource.dart';
 import 'package:coffee_app_vgv/domain/model/coffee_model.dart';
 import 'package:coffee_app_vgv/presentation/favorite/export_favorite.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FavoriteCubit extends Cubit<FavoriteState> {
-  FavoriteCubit() : super(InitialFavoriteState());
+  final ILocalDataSource localDataSource;
+
+  FavoriteCubit(this.localDataSource) : super(InitialFavoriteState()) {
+    fetchFavorites();
+  }
 
   Future<void> fetchFavorites() async {
     try {
-      final favorites = state.favorites;
+      final favorites = await localDataSource.getAll();
 
       if (favorites.isEmpty) {
         emit(InitialFavoriteState());
         return;
       }
+
       emit(LoadFavoriteState(favorites: favorites));
     } catch (e) {
       emit(ErrorFavoriteState(message: e.toString()));
@@ -21,14 +27,44 @@ class FavoriteCubit extends Cubit<FavoriteState> {
 
   Future<void> addFavorite(CoffeeModel coffee) async {
     try {
-      final updatedFavorites = List<CoffeeModel>.from(state.favorites)
-        ..add(coffee);
+      await localDataSource.save(coffee.bytes!);
+
+      final updatedFavorites = await localDataSource.getAll();
 
       if (updatedFavorites.isEmpty) {
         emit(InitialFavoriteState());
         return;
       }
+
       emit(LoadFavoriteState(favorites: updatedFavorites));
+
+      fetchFavorites();
+    } catch (e) {
+      emit(ErrorFavoriteState(message: e.toString()));
+    }
+  }
+
+  Future<void> removeFavorite(String localPath) async {
+    try {
+      await localDataSource.remove(localPath);
+
+      final updatedFavorites = await localDataSource.getAll();
+
+      if (updatedFavorites.isEmpty) {
+        emit(InitialFavoriteState());
+        return;
+      }
+
+      emit(LoadFavoriteState(favorites: updatedFavorites));
+    } catch (e) {
+      emit(ErrorFavoriteState(message: e.toString()));
+    }
+  }
+
+  Future<void> clearFavorites() async {
+    try {
+      await localDataSource.clear();
+      emit(InitialFavoriteState());
     } catch (e) {
       emit(ErrorFavoriteState(message: e.toString()));
     }
