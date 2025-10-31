86e310c7c12b6505e1c9bde0431b6e410567e29a
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-10-30 22:19:38 -0300

    refactor: changed DI to use abstractions for repository and HTTP client

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,10 @@
+import 'package:coffee_app_vgv/presentation/favorite/export_favorite.dart';
+import 'package:coffee_app_vgv/presentation/home/export_home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'core/http_client.dart';
+import 'domain/coffee_repository.dart';
 import 'presentation/navigation/export_navigation.dart';
 
 class App extends StatelessWidget {
@@ -9,9 +13,25 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider(
-        create: (context) => NavigationCubit(),
-        child: NavigationView(),
+      home: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<IHttpClient>(create: (context) => HttpClient()),
+          RepositoryProvider<ICoffeeRepository>(
+            create: (context) =>
+                CoffeeRepository(httpClient: context.read<IHttpClient>()),
+          ),
+        ],
+        child: MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (context) => NavigationCubit()),
+            BlocProvider(
+              create: (context) =>
+                  HomeCubit(repository: context.read<ICoffeeRepository>()),
+            ),
+            BlocProvider(create: (context) => FavoriteCubit()),
+          ],
+          child: NavigationView(),
+        ),
       ),
     );
   }
