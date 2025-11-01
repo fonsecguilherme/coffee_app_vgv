673f7b9b96bfc0f168cbe926b8e505c9ca67590f
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-11-01 18:07:06 -0300

    feat: implementation of local notifications with images

--- lib/core/notification_service.dart
+++ lib/core/notification_service.dart
@@ -1,9 +1,16 @@
+import 'package:coffee_app_vgv/data/datasource/local_coffee_datasource.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationService {
+  final ILocalDataSource localDataSource;
+
+  NotificationService({required this.localDataSource});
+
   final notificationPlugin = FlutterLocalNotificationsPlugin();
 
   final bool _isInitialized = false;
+
+  final List<String> localImages = [];
 
   Future<void> initiNotification() async {
     if (_isInitialized) return;
@@ -33,17 +40,57 @@ class NotificationService {
     await androidImplementation?.requestNotificationsPermission();
   }
 
-  NotificationDetails notificationDetails() {
-    return const NotificationDetails(
-      android: AndroidNotificationDetails(
+  Future<void> fetchCoffee() async {
+    final images = await localDataSource.getAll();
+
+    for (var image in images) {
+      localImages.add(image.localPath!);
+    }
+  }
+
+  Future<NotificationDetails> notificationDetails() async {
+    await fetchCoffee();
+
+    if (localImages.isEmpty) {
+      const androidDetails = AndroidNotificationDetails(
         'daily_chanel_id',
-        'DAily notifications',
-        channelDescription: 'Daily coffee sugestion',
+        'Daily notifications',
+        channelDescription: 'Daily coffee suggestion',
         importance: Importance.max,
         priority: Priority.max,
-      ),
-      iOS: DarwinNotificationDetails(),
+      );
+
+      return const NotificationDetails(
+        android: androidDetails,
+        iOS: DarwinNotificationDetails(),
+      );
+    }
+
+    final randomImage = await localDataSource.getSingleImagePath();
+
+    final bigPicture = BigPictureStyleInformation(
+      FilePathAndroidBitmap(randomImage),
+      largeIcon: FilePathAndroidBitmap(randomImage),
     );
+
+    return NotificationDetails(
+      android: AndroidNotificationDetails(
+        'daily_chanel_id',
+        'Daily notifications',
+        channelDescription: 'Daily coffee suggestion',
+        importance: Importance.max,
+        priority: Priority.max,
+        styleInformation: bigPicture,
+      ),
+      iOS: DarwinNotificationDetails(
+        attachments: [DarwinNotificationAttachment(randomImage)],
+      ),
+    );
+  }
+
+  checkForNotifications() async {
+    final details = await notificationPlugin.getNotificationAppLaunchDetails();
+    if (details != null && details.didNotificationLaunchApp) {}
   }
 
   Future<void> showNotification({
@@ -51,6 +98,8 @@ class NotificationService {
     String title = 'It\'s coffee time!',
     String body = 'Have you tried this one?',
   }) async {
-    return notificationPlugin.show(id, title, body, notificationDetails());
+    final details = await notificationDetails();
+
+    return notificationPlugin.show(id, title, body, details);
   }
 }
