33329b562e6c296da528aef24e51916cca793454
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-10-31 21:42:40 -0300

    feat: add new properties to coffee model

--- lib/domain/model/coffee_model.dart
+++ lib/domain/model/coffee_model.dart
@@ -1,17 +1,22 @@
-import 'dart:convert';
+import 'dart:typed_data';
 
 class CoffeeModel {
   final String file;
+  final String? localPath;
+  final Uint8List? bytes;
 
-  CoffeeModel({required this.file});
-
-  factory CoffeeModel.fromRawJson(String str) =>
-      CoffeeModel.fromJson(json.decode(str));
-
-  String toRawJson() => json.encode(toJson());
+  CoffeeModel({required this.file, this.localPath, this.bytes});
 
   factory CoffeeModel.fromJson(Map<String, dynamic> json) =>
-      CoffeeModel(file: json["file"]);
+      CoffeeModel(file: json["file"], localPath: null, bytes: json["bytes"]);
 
-  Map<String, dynamic> toJson() => {"file": file};
+  Map<String, dynamic> toJson() => {"file": file, "localPath": localPath, "bytes": bytes};
+
+  CoffeeModel copyWith({String? file, String? localPath, Uint8List? bytes}) {
+    return CoffeeModel(
+      file: file ?? this.file,
+      localPath: localPath ?? this.localPath,
+      bytes: bytes ?? this.bytes,
+    );
+  }
 }
