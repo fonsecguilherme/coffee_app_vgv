08774e9f4577e134cbb4dbeec1f7809568bbea20
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-11-01 18:06:27 -0300

    refactor: moved app DI to main file to initialize notification service in the start of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,45 @@
-import 'package:flutter/material.dart';
+import 'package:coffee_app_vgv/app.dart';
+import 'package:coffee_app_vgv/presentation/favorite/export_favorite.dart';
+import 'package:coffee_app_vgv/presentation/home/export_home.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'app.dart';
+import 'core/http_client.dart';
 import 'core/notification_service.dart';
+import 'data/datasource/local_coffee_datasource.dart';
+import 'domain/coffee_repository.dart';
+import 'domain/repositories/i_coffee_repository.dart';
+import 'domain/service/i_http_client.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  NotificationService().initiNotification();
+  final httpClient = HttpClient();
+  final localDataSource = LocalCoffeeDataSource();
+  final coffeeRepository = CoffeeRepository(httpClient: httpClient);
+  final notificationService = NotificationService(
+    localDataSource: localDataSource,
+  );
 
-  runApp(const App());
+  await notificationService.initiNotification();
+
+  runApp(
+    MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<IHttpClient>.value(value: httpClient),
+        RepositoryProvider<ICoffeeRepository>.value(value: coffeeRepository),
+        RepositoryProvider<ILocalDataSource>.value(value: localDataSource),
+        RepositoryProvider<NotificationService>.value(
+          value: notificationService,
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => HomeCubit(repository: coffeeRepository)),
+          BlocProvider(create: (_) => FavoriteCubit(localDataSource)),
+        ],
+        child: const App(),
+      ),
+    ),
+  );
 }
