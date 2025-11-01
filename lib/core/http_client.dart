cab485711b6c46248a3cc8755606e7f34259d22a
Guilherme Fonseca <guilherme.ramos@arapiraca.ufal.br>
2025-10-31 21:42:09 -0300

    refactor: http client

--- lib/core/http_client.dart
+++ lib/core/http_client.dart
@@ -1,14 +1,22 @@
 import 'package:http/http.dart' as http;
 
-abstract class IHttpClient {
-  Future get({required String url});
-}
+import '../domain/service/i_http_client.dart';
 
 class HttpClient implements IHttpClient {
   final client = http.Client();
 
   @override
-  Future get({required String url}) async {
-    return await client.get(Uri.parse(url));
+  Future<HttpResponse> get({required String url}) async {
+    try {
+      final response = await client.get(Uri.parse(url));
+
+      return HttpResponse(
+        body: response.body,
+        statusCode: response.statusCode,
+        bodyBytes: response.bodyBytes,
+      );
+    } catch (e) {
+      throw Exception('An error happened: $e');
+    }
   }
 }
